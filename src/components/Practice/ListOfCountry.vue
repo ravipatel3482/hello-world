585f8921b183ac48cbae2afae0da55d7c5217ce3
Ravi Patel <ravipatel.3482@gmail.com>
2026-04-22 08:40:23 -0400

    fix the filter logic

--- src/components/Practice/ListOfCountry.vue
+++ src/components/Practice/ListOfCountry.vue
@@ -23,8 +23,8 @@
                 <label class="form-label mb-0 fw-bold">Select Region</label>
               </div>
               <div class="col">
-                <select v-model="region" class="form-select shadow-sm" @change="handleSelectionChange()">
-                  <option :value="''">All Regions</option> <option v-for="(reg, index) in uniqueregion" :key="index" :value="reg">
+                <select v-model="region" class="form-select shadow-sm">
+                  <option :value="''">All Regions</option> <option v-for="reg in uniqueregion" :key="reg" :value="reg">
                     {{ reg }}
                   </option>
                 </select>
@@ -36,7 +36,7 @@
       <div class="row" v-if="loading">Loading...
       </div>
       <div class="row" v-else>
-        <div  class="col-md-4 mb-4"  v-for="country in filteredCountry"   :key="country.region">
+        <div  class="col-md-4 mb-4"  v-for="country in countryList"   :key="country.name.common">
           <div class="card h-100 shadow-sm">
             <div class="card-body">
               <h5 class="card-title">
@@ -74,24 +74,21 @@ import Country_List from '@/stubData/countryList.js'
       return {
         countryList:[],
         loading:false,
-        filteredCountry:[],
         region:'',
         uniqueregion:[]
       }
     },
     methods: {
-        handleSelectionChange() {
-          // If no region is selected (null or empty string), show all countries
-          if (!this.region) {
-            this.filteredCountry = this.countryList;
-            return;
-          }
-          
-          // Filter the original list based on the selected region
-          this.filteredCountry = this.countryList.filter(it => it.region === this.region);
-          console.log("Filtered countries:", this.filteredCountry);
+       
+    },
+    computed: {
+      filteredCountry() {
+        if (!this.region) {
+          return this.countryList
         }
-      },
+        return this.countryList.filter(c => c.region === this.region)
+      }
+    },
     
      async mounted(){ 
       this.loading = true
@@ -105,25 +102,18 @@ import Country_List from '@/stubData/countryList.js'
              
             this.countryList = await response.json()
             console.log(this.countryList)
-            this.filteredCountry = this.countryList,
-            this.uniqueregion = [...new Set(this.filteredCountry.map(item => item.region))];
+           
+            this.uniqueregion = [...new Set(this.countryList.map(item => item.region))];
             console.log( "region =  + "  +  this.uniqueregion)
             this.loading = false
           }
           catch(err){
               console.error('API failed, using stub data:', err)
               this.countryList = Country_List
-              this.filteredCountry = Country_List
               this.error =  err
           }
-      },
-      watch: {
-        selectedId(newValue, oldValue) {
-          // This runs whenever selectedId changes, even programmatically
-          console.log(newValue)
-          console.log(oldValue)
-        }
-      }   
+      }
+    
   }
 </script>
 
